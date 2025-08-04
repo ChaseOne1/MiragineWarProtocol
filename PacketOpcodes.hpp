b102d375d4ee7c95db0b195175cdf0bdbbc86390
ChaseOne1 <chaseone1@foxmail.com>
2025-08-05 02:03:40 +0800

    feat: Add MatchmakingReq&Rsp protos

--- PacketOpcodes.hpp
+++ PacketOpcodes.hpp
@@ -4,6 +4,8 @@ namespace proto {
 enum PacketOpcode : unsigned char
 {
     ID_USER_PACKET_ENUM = 134,
-    ID_SHOW_MESSAGE_NOTIFY
+    ID_SHOW_MESSAGE_NOTIFY,
+    ID_MATCHMAKING_REQ,
+    ID_MATCHMAKING_RSP,
 };
 }
